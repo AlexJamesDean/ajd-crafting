4c7eda7ad4f44b340c42d81f732bd0274f1a32a4
AlexJamesDean <lead@alexjamesdean.co.uk>
2025-11-19 09:37:48 +0000

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,10 +1,11 @@
 fx_version 'cerulean'
 game 'gta5'
 
-author 'AJD Development'
+author 'AlexJamesDean'
 description 'Promotional repository for AJD Crafting. Purchase the full QBCore crafting framework on Tebex.'
 version '1.0.0'
 
 -- No runtime scripts are included in this promo repo.
 -- Grab the complete resource with all source files from https://alexjamesdean.tebex.io/.
+
 
